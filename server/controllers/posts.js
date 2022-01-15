6cd7487b6d71901c582fbc27b483146f18f71b0b
Asaf <benyeffeta@gmail.com>
2022-01-15 16:28:01 +0200

    first commit

---
+++ server/controllers/posts.js
@@ -0,0 +1,71 @@
+import PostMessage from "../models/postMessage.js";
+import mongoose from "mongoose";
+
+export const getPosts =  async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    console.log(postMessages);
+    res.status(200).json(postMessages);
+
+  } catch (error) {
+    res.status(404).json({message:error.message});
+  }
+}
+
+export const createPost = async (req, res)=>{
+  
+  const post = req.body;
+  
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+
+    res.status(201).json(newPost);
+
+  } catch (error) {
+    res.status(409).json({message: error.message});
+  }
+};
+
+export const updatePost = async (req, res)=>{
+  
+  // destruct and rename as _id
+  const {id: _id} = req.params;
+  const post = req.body;
+
+  // validate _id is really mongoose ID
+  if (!mongoose.Types.ObjectId.isValid(_id))  {
+    return res.status(404).
+    send('No post with that id');
+  }
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true });
+ 
+  res.json(updatedPost);
+}; 
+
+export const deletePost = async(req, res) => {
+
+  const {id} = req.params;
+  // validate _id is really mongoose ID
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id'); 
+
+  await PostMessage.findByIdAndRemove(id);
+
+  console.log('DELETE!');
+  res.json({message: 'Post deleted successfully'});
+}
+
+export const likePost = async(req, res) => {
+
+  const {id} = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id'); 
+
+  const post = await PostMessage.findById(id);
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount + 1}, { new: true });
+
+  res.json(updatedPost);
+}
+  
